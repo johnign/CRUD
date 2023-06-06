ae595eb617ce790a03489ce0096726b8c841f601
johnign <johnsonkvpt@gmail.com>
2023-06-06 17:41:50 +0530

    user profile

--- src/pages/AddEditUser.js
+++ src/pages/AddEditUser.js
@@ -1,7 +1,9 @@
 import React, {useState, useEffect} from 'react'
 import {Button, Form, Grid, Loader} from "semantic-ui-react";
-import {storage} from "../firebase";
+import { storage, db } from "../firebase";
 import {useParams, useNavigate} from "react-router-dom";
+import { getDownloadURL, ref, uploadBytesResumable} from "firebase/storage";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 const initialState = {
      name: "",
@@ -11,11 +13,45 @@ const initialState = {
 }
 const AddEditUser = () => {
   const [data, setData] = useState(initialState);
-  const {name, email, info, contact} = data;
+  const { name, email, info, contact } = data;
   const [file, setFile] = useState(null);
   const [progress, setProgress] = useState(null);
   const [errors, setErrors] = useState({});
   const [isSubmit, setIsSubmit] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() =>{
+    const uploadFile = () => {
+      const name = new Date().getTime() + file.name;
+      const storageRef = ref(storage,file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on("state_changed", (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setProgress(progress);
+        switch (snapshot.state) {
+          case "paused":
+              console.log("Upload is Pause");
+              break;
+          case "running":
+            console.log("Upload is Running");
+              break;
+          default:
+            break;
+        }
+      }, (error) => {
+          console.log(error)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setData((prev) => ({...prev, img: downloadURL})); 
+        });
+      }
+      );
+    };
+
+    file && uploadFile()
+  }, [file])
   
   const handleChange = (e) => {
      setData({...data, [e.target.name]: e.target.value })
@@ -26,18 +62,30 @@ const AddEditUser = () => {
      if(!name) {
        errors.name = "Name is Required"
       }
-      if(!name) {
-        errors.name = "Name is Required"
+      if(!email) {
+        errors.email = "Email is Required"
       }
-      if(!name) {
-        errors.name = "Name is Required"
+      if(!info) {
+        errors.info = "Info is Required"
       }
-  }
+      if(!contact) {
+        errors.contact = "Contact is Required"
+      }
+      return errors;
+  };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     let errors = validate();
-  }
+    if(Object.keys(errors).length) return setErrors(errors);
+    setIsSubmit(true);
+      await addDoc(collection(db, "users"),
+      {
+        ...data,
+        timestamp: serverTimestamp()
+      })
+      navigate("/");
+  };
   return (
     <div>
       <Grid 
@@ -57,6 +105,7 @@ const AddEditUser = () => {
                     <Form onSubmit={handleSubmit}>
                      <Form.Input 
                       label="Name" 
+                      error={errors.name ? {content: errors.name} : null}
                       placeHolder="Enter Name"
                       name="name"
                       onChange={handleChange}
@@ -65,6 +114,7 @@ const AddEditUser = () => {
                      />
                      <Form.Input 
                       label="Email" 
+                      error={errors.email ? {content: errors.email} : null}
                       placeHolder="Enter Email"
                       name="email"
                       onChange={handleChange}
@@ -72,6 +122,7 @@ const AddEditUser = () => {
                      />
                      <Form.TextArea 
                       label="Info" 
+                      error={errors.info ? {content: errors.info} : null}
                       placeHolder="Enter Info"
                       name="info"
                       onChange={handleChange}
@@ -79,6 +130,9 @@ const AddEditUser = () => {
                      />
                      <Form.Input 
                       label="Contact" 
+                      error={
+                        errors.contact ? { content: errors.contact } : null
+                      }
                       placeHolder="Enter Contact"
                       name="contact"
                       onChange={handleChange}
@@ -89,6 +143,12 @@ const AddEditUser = () => {
                      type="file"
                      onChange={(e) => setFile(e.target.files[0])}
                      />
+                     <Button 
+                     primary 
+                     type="submit" 
+                     >
+                      Submit
+                      </Button>
                     </Form>
                   </>
                   )}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { db } from "../firebase";
+import { Button, Card, Grid, Container, Image } from "semantic-ui-react";
+import { useNavigate } from "react-router-dom";
+import { collection, onSnapshot } from "firebase/firestore";
 
-const home = () =>{
-  console.log("hello");
+const home = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    setLoading(true);
+    const unsub = onSnapshot
+    (collection(db, "users"), 
+    (snapshot) => {
+        let list =[];
+        snapshot.docs.forEach((doc) => {
+          list.push({id: doc.id, ...doc.data()})
+        });
+        setUsers(list);
+        setLoading(false);
+      }, 
+      (error) => {
+          console.log(error);
+    });
+    return () => {
+      unsub();
+    }
+  }, []);
   return (
-    <div>Home</div>
+     <Container>
+          <Card.Group>
+            <Grid columns={3} stackable>
+              {users && users.map((item) => (
+                 <Grid.Column>
+                      <Card key={item.id}>
+                        <Card.Content>
+                          <Image 
+                          src={item.img}
+                          size="medium"
+                          style={{
+                            height: "150px",
+                            width: "150px",
+                            borderRadius: "50%",
+                          }}
+                          />
+                          <Card.Header style={{marginTop: "10px" }}>
+                            {item.name}
+                            </Card.Header>
+                          <Card.Description>{item.info}</Card.Description>
+                        </Card.Content>
+                        <Card.Content extra>
+                          <div>
+                            <Button 
+                            color="green" 
+                            onClick={() => navigate('/update/${item.id}')}
+                            >
+                              Update
+                            </Button>
+                            <Button 
+                            color="purple">View</Button>
+                          </div>
+
+                        </Card.Content>
+                      </Card>
+                 </Grid.Column>
+              ))}
+
+            </Grid>
+          </Card.Group>
+     </Container>
   )
 }
 export  default home;
